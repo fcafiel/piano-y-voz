b4356b5c90632b47c83464ad6f410a7f260af025
Frank Cafiel <fcaffield2@gmail.com>
2017-10-10 22:47:19 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        <img src="assets/images/logo-piano-y-voz.png" alt="Piano y Voz">
+      </div>
+      <div class="contrataciones">
+        <h2>Contrataciones</h2>
+        <p><a href="tel:+573114184907"><strong>311</strong>4184907</a></p>
+        <p><a href="tel:+573044046910"><strong>304</strong>4046910</a></p>
+      </div>
+
+    </header>
+    <section class="hero">
+      <div class="columnas">
+        <div class="izquierda">
+          <h1>Que tu Momento Especial <span class="main">SEA PERFECTO</span> con <span class="strong">nuestra música</span></h1>
+          <p class="intro">Quisquam expedita blanditiis magnam mollit, veritatis, curabitur commodo laoreet class, sagittis, hymenaeos arcu quas faucibus gravida nisl placerat quis vel assumenda repellendus etiam maxime gravida delectus illum pariatur</p>
+          <p class="copy">Integer eos asperiores voluptatem!</p>
+        </div>
+        <div class="derecha">
+          <div class="contacto">
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>Fugit convallis aute esse, aspernatur, fugiat repellendus.</p>
+            <form class="contacto" action="index.html" method="post">
+              <span>
+                <input type="text" name="nombres" placeholder="Nombres">
+              </span>
+              <span>
+                <input type="text" name="email" placeholder="Correo electrónico">
+              </span>
+              <span>
+                <input type="text" name="telefono" placeholder="Teléfono">
+              </span>
+              <span>
+                <select name="evento">
+                  <option value="">-- Evento --</option>
+                  <option value="aniversario">Aniversario</option>
+                  <option value="bautizo">Bautizo</option>
+                  <option value="matrimonio">Matrimonio</option>
+                  <option value="misa">Misa</option>
+                  <option value="primera_comunion">Primera Comunión</option>
+                  <option value="quinceanos">Quinceaños</option>
+                  <option value="sepelio">Sepelio</option>
+                </select>
+              </span>
+              <span class="cta">
+                <input type="submit" value="Enviar">
+              </span>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="presentacion">
+      <h2>Acompañamiento musical de Celebraciones Eucarísticas</h2>
+      <p>Amet porro cubilia autem omnis hic soluta nonummy lacus leo vehicula rem quaerat ipsam! Totam sed, facere sunt libero sapiente? Eum odit auctor expedita, voluptates laboriosam pellentesque numquam ultrices, aliquam, elementum quaerat.</p>
+    </section>
+    <section class="servicios">
+      <div class="slider">
+        <div class="sliding">
+          <div class="slide">
+            <img src="" alt="">
+            <div class="info">
+              <h3>Bautizos</h3>
+              <p>Acompañamiento musical para bautizos</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="" alt="">
+            <div class="info">
+              <h3>Bautizos</h3>
+              <p>Acompañamiento musical para bautizos</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="" alt="">
+            <div class="info">
+              <h3>Bautizos</h3>
+              <p>Acompañamiento musical para bautizos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="integrantes">
+      <img src="" alt="">
+      <h2>Quiénes somos</h2>
+      <h3>Frank Cafiel <img src="" alt=""> Ayda Castillo</h3>
+      <p>Juntos contamos con <strong>más de 10 años de experiencia</strong> en el acompañamiento musical de celebraciones eucarísticas en distintos momentos y escenarios.</p>
+      <p>Esta experiencia nos acredita para brindar un <strong>servicio de calidad</strong> acorde a los Momentos Especiales que nuestros clientes requieran.</p>
+    </section>
+    <footer>
+      <div class="columnas">
+        <div class="col">
+          <img src="" alt="">
+        </div>
+        <div class="col">
+          <div class="whatsapp">
+            <img src="" alt=""> <a href="#">311.4184907</a> - <a href="#">304.4046910</a>
+          </div>
+          <div class="email">
+            <a href="mailto:contacto@pianoyvoz.co">contacto@pianoyvoz.co</a>
+          </div>
+        </div>
+        <div class="col">
+          <a href="#"><img src="" alt=""></a>
+        </div>
+      </div>
+      <div class="copy">
+        <p>Copyright &copy; 2017 | <a href="http://www.pianoyvoz.co/">www.<strong>PianoyVoz.co</strong></a></p>
+      </div>
+    </footer>
+
+  </body>
+</html>
