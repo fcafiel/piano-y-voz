3d726f327b5b6fb8f7dd73c7ebffb27e5337b8c7
Frank Cafiel <fcaffield2@gmail.com>
2017-10-14 22:27:37 -0500

    Hero y presentación

--- index.php
+++ index.php
@@ -1,119 +1,159 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+
+    <title>Piano y Voz Bogotá</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
   </head>
   <body>
     <header>
+      <div class="col"></div>
       <div class="logo">
-        <img src="assets/images/logo-piano-y-voz.png" alt="Piano y Voz">
+        <img src="assets/images/logo-piano-y-voz.png" alt="Piano y Voz" width="280">
       </div>
       <div class="contrataciones">
-        <h2>Contrataciones</h2>
-        <p><a href="tel:+573114184907"><strong>311</strong>4184907</a></p>
-        <p><a href="tel:+573044046910"><strong>304</strong>4046910</a></p>
+        <h2>CONTRATACIONES</h2>
+        <p><a href="tel:+573114184907"><strong>311 </strong>418 4907</a></p>
+        <p><a href="tel:+573044046910"><strong>304 </strong>404 6910</a></p>
       </div>
 
     </header>
     <section class="hero">
-      <div class="columnas">
-        <div class="izquierda">
-          <h1>Que tu Momento Especial <span class="main">SEA PERFECTO</span> con <span class="strong">nuestra música</span></h1>
-          <p class="intro">Quisquam expedita blanditiis magnam mollit, veritatis, curabitur commodo laoreet class, sagittis, hymenaeos arcu quas faucibus gravida nisl placerat quis vel assumenda repellendus etiam maxime gravida delectus illum pariatur</p>
-          <p class="copy">Integer eos asperiores voluptatem!</p>
-        </div>
-        <div class="derecha">
-          <div class="contacto">
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Fugit convallis aute esse, aspernatur, fugiat repellendus.</p>
-            <form class="contacto" action="index.html" method="post">
-              <span>
-                <input type="text" name="nombres" placeholder="Nombres">
-              </span>
-              <span>
-                <input type="text" name="email" placeholder="Correo electrónico">
-              </span>
-              <span>
-                <input type="text" name="telefono" placeholder="Teléfono">
-              </span>
-              <span>
-                <select name="evento">
-                  <option value="">-- Evento --</option>
-                  <option value="aniversario">Aniversario</option>
-                  <option value="bautizo">Bautizo</option>
-                  <option value="matrimonio">Matrimonio</option>
-                  <option value="misa">Misa</option>
-                  <option value="primera_comunion">Primera Comunión</option>
-                  <option value="quinceanos">Quinceaños</option>
-                  <option value="sepelio">Sepelio</option>
-                </select>
-              </span>
-              <span class="cta">
-                <input type="submit" value="Enviar">
-              </span>
-            </form>
+      <div class="inner">
+        <div class="columnas">
+          <div class="izquierda">
+            <h1>Que tu Momento Especial <span class="main">SEA PERFECTO</span> <span class="strong">con <strong>nuestra música</strong></span></h1>
+            <p class="intro">Quisquam expedita blanditiis magnam mollit, veritatis, curabitur commodo laoreet class, sagittis, hymenaeos arcu quas faucibus gravida nisl placerat quis vel assumenda repellendus etiam maxime gravida delectus illum pariatur</p>
+            <p class="copy">Integer eos asperiores voluptatem!</p>
+          </div>
+          <div class="derecha">
+            <div class="contacto">
+              <h2>Lorem ipsum dolor sit amet</h2>
+              <p>Fugit convallis aute esse, aspernatur, fugiat repellendus.</p>
+              <form action="index.html" method="post">
+                <span>
+                  <input type="text" name="nombres" placeholder="Nombres">
+                </span>
+                <span>
+                  <input type="text" name="email" placeholder="Correo electrónico">
+                </span>
+                <span>
+                  <input type="text" name="telefono" placeholder="Teléfono">
+                </span>
+                <span>
+                  <select name="evento">
+                    <option value="">-- Evento --</option>
+                    <option value="aniversario">Aniversario</option>
+                    <option value="bautizo">Bautizo</option>
+                    <option value="matrimonio">Matrimonio</option>
+                    <option value="misa">Misa</option>
+                    <option value="primera_comunion">Primera Comunión</option>
+                    <option value="quinceanos">Quinceaños</option>
+                    <option value="sepelio">Sepelio</option>
+                  </select>
+                </span>
+                <span class="cta">
+                  <input type="submit" value="ENVIAR">
+                </span>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </section>
     <section class="presentacion">
-      <h2>Acompañamiento musical de Celebraciones Eucarísticas</h2>
-      <p>Amet porro cubilia autem omnis hic soluta nonummy lacus leo vehicula rem quaerat ipsam! Totam sed, facere sunt libero sapiente? Eum odit auctor expedita, voluptates laboriosam pellentesque numquam ultrices, aliquam, elementum quaerat.</p>
+      <div class="inner">
+        <h2>Acompañamiento musical de Celebraciones Eucarísticas</h2>
+        <p>Amet porro cubilia autem omnis hic soluta nonummy lacus leo vehicula rem quaerat ipsam! Totam sed, facere sunt libero sapiente? Eum odit auctor expedita, voluptates laboriosam pellentesque numquam ultrices, aliquam, elementum quaerat.</p>
+        <p class="boton"><a href="#" class="cta">Contáctanos</a></p>
+    </div>
     </section>
     <section class="servicios">
       <div class="slider">
         <div class="sliding">
           <div class="slide">
-            <img src="" alt="">
+            <img src="assets/images/matrimonios.jpg" alt="Misas para matrimonios">
             <div class="info">
-              <h3>Bautizos</h3>
-              <p>Acompañamiento musical para bautizos</p>
+              <h3>Matrimonios</h3>
+              <p>Una nueva etapa en la vida empieza con la bendición de Dios. Que este momento único y especial en pareja sea perfecto; acompáñalo de las mejores canciones.</p>
             </div>
           </div>
           <div class="slide">
-            <img src="" alt="">
+            <img src="assets/images/aniversarios.jpg" alt="Misas de aniversarios">
             <div class="info">
-              <h3>Bautizos</h3>
-              <p>Acompañamiento musical para bautizos</p>
+              <h3>Aniversarios</h3>
+              <p>Bodas de Oro, de Diamante, de Plata, de Papel..... Con el paso de los años las parejas se hacen más unidas. Celebre esos años con la bendición de Dios y el mejor acompañamiento musical.</p>
             </div>
           </div>
           <div class="slide">
-            <img src="" alt="">
+            <img src="assets/images/bautizos.jpg" alt="Misas para bautizos">
             <div class="info">
               <h3>Bautizos</h3>
-              <p>Acompañamiento musical para bautizos</p>
+              <p>Ese primer momento de encuentro con Dios debe ser perfecto, y nuestra música puede hacerlo aún más especial.</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="assets/images/primera-comunion.jpg" alt="Misas para primera comunión">
+            <div class="info">
+              <h3>Primera Comunión</h3>
+              <p>Acompañamiento musical para Primera Comunión. El primer encuentro con el Cuerpo y Sangre de Cristo es un momento especial que debe ser ambientado con las canciones perfectas.</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="assets/images/quinceanos.jpg" alt="Misas para quinceaños">
+            <div class="info">
+              <h3>Quinceaños</h3>
+              <p>El cambio de niña a mujer debe llevar la Gracia de Dios, y la música puede hacer más especial ese momento. Acompañamiento musical para Misas de Quinceañeras.</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="assets/images/misas.jpg" alt="Misa cantada">
+            <div class="info">
+              <h3>Misas</h3>
+              <p>Acompañamiento musical para Eucaristías. El encuentro con Dios es más especial acompañado de las mejores canciones. "El que canta ora dos veces".</p>
+            </div>
+          </div>
+          <div class="slide">
+            <img src="assets/images/sepelios.jpg" alt="Misas para sepelios">
+            <div class="info">
+              <h3>Sepelios</h3>
+              <p>Acompañamos musicalmente Misas para Sepelios. La música adecuada ayuda a reconfortar un corazón triste.</p>
             </div>
           </div>
         </div>
       </div>
     </section>
     <section class="integrantes">
-      <img src="" alt="">
-      <h2>Quiénes somos</h2>
-      <h3>Frank Cafiel <img src="" alt=""> Ayda Castillo</h3>
-      <p>Juntos contamos con <strong>más de 10 años de experiencia</strong> en el acompañamiento musical de celebraciones eucarísticas en distintos momentos y escenarios.</p>
-      <p>Esta experiencia nos acredita para brindar un <strong>servicio de calidad</strong> acorde a los Momentos Especiales que nuestros clientes requieran.</p>
+      <div class="inner">
+        <img src="assets/images/piano-y-voz.jpg" alt="Integrantes de Piano y Voz">
+        <h2>Quiénes somos</h2>
+        <h3>Frank Cafiel <img src="assets/images/clave-sol-piano-y-voz.png" alt="Piano y Voz"> Ayda Castillo</h3>
+        <p>Juntos contamos con <strong>más de 10 años de experiencia</strong> en el acompañamiento musical de celebraciones eucarísticas en distintos momentos y escenarios.</p>
+        <p>Esta experiencia nos acredita para brindar un <strong>servicio de calidad</strong> acorde a los Momentos Especiales que nuestros clientes requieran.</p>
+      </div>
     </section>
     <footer>
       <div class="columnas">
         <div class="col">
-          <img src="" alt="">
+          <img src="assets/images/logo-piano-y-voz-blanco.png" alt="Piano y Voz">
         </div>
         <div class="col">
           <div class="whatsapp">
-            <img src="" alt=""> <a href="#">311.4184907</a> - <a href="#">304.4046910</a>
+            <img src="assets/images/whatsapp.png" alt="Whatsapp"> <a href="tel:+573114184907">311.4184907</a> - <a href="#">304.4046910</a>
           </div>
           <div class="email">
-            <a href="mailto:contacto@pianoyvoz.co">contacto@pianoyvoz.co</a>
+            <img src="assets/images/mail.png" alt="Email"> <a href="mailto:contacto@pianoyvoz.co" title="Contáctanos">contacto@pianoyvoz.co</a>
           </div>
         </div>
         <div class="col">
-          <a href="#"><img src="" alt=""></a>
+          <a href="https://www.facebook.com/PianoyVoz.Bogota/" target="_blank" title="Síguenos"><img src="assets/images/facebook.png" alt="Facebook"></a>
         </div>
       </div>
       <div class="copy">
-        <p>Copyright &copy; 2017 | <a href="http://www.pianoyvoz.co/">www.<strong>PianoyVoz.co</strong></a></p>
+        <p>Copyright &copy; 2017 | <a href="http://www.pianoyvoz.co/" title="Piano y Voz">www.<strong>PianoyVoz.co</strong></a></p>
       </div>
     </footer>
 
